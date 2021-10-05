4b00af8241ffc1b29ab9e5c224d9254ff08c2e96
Rob <rob.eagleheart@gmail.com>
2021-10-05 16:15:02 +0000

    Walkthrough – Entering Players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,10 @@
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player, with: 'Charlotte'
+    fill_in :player2, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Charlotte vs. Mittens'
   end
+  
 end
